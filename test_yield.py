1a37ea0d76283bfbbed04c79bc1846bbea20ee13
Morteza Zakeri <m-zakeri@live.com>
2018-03-26 00:30:43 +0430

    change for pull

--- test_yield.py
+++ test_yield.py
@@ -17,3 +17,16 @@ for i in range(10):
     print(next(x))
 
 # A comment for test ...
+
+
+def sample_function():
+    """ The sample function doc string
+      # Arguments
+        lr: float >= 0. Learning rate.
+        rho: float >= 0.
+        epsilon: float >= 0. Fuzz factor.
+        decay: float >= 0. Learning rate decay over each update.
+    """
+    pass
+
+print(sample_function.__doc__)
