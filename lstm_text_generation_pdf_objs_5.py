a1162aae0744feb14e9927f7be4139f3e5bcbf6a
Morteza Zakeri <m-zakeri@live.com>
2018-05-12 00:40:45 +0430

    Change optmizer to Adam. dropout=0.3 to model_6, Set model=model_6, batch_size=128, lr=0.01. 13970221

--- lstm_text_generation_pdf_objs_5.py
+++ lstm_text_generation_pdf_objs_5.py
@@ -169,8 +169,8 @@ class FileFormatFuzzer(object):
 
         print('Build and compile model ...')
         model, model_name = self.define_model((self.maxlen, len(self.chars)), len(self.chars))
-        optimizer = RMSprop(lr=0.01)  # [0.001, 0.01, 0.02, 0.05, 0.1]
-        # optimizer = Adam()
+        # optimizer = RMSprop(lr=0.01)  # [0.001, 0.01, 0.02, 0.05, 0.1]
+        optimizer = Adam()
         model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
         print(model_name, ' summary ...')
